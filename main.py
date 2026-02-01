8a97098a1d1eb40103cebd9678b05d0d9d533bf4
Misam Karim <misamkarim@eng.rizvi.edu.in>
2026-02-01 23:29:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 # AIM: Design a Python program to compute 
 # the factorial of a given integer N.
-# Coder:
-# Date:
+# Coder: Misam Karim
+# Date: 30/1/26
 
 print("--- Factorial Finder ---\n")
 
 
 # Write your code here
+n = int(input("Enter Number: "))
+fact = 1
+if n<0:
+    print(f"Factorial of {n} is Not Defined")
+else:
+    for i in range(1,n+1):
+        fact *= i
+    print(f"Factorial of {n} is {fact}")
